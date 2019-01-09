a49c745546a213696fa8a9efd631ee61f8b8d3d3
TheProgrammingKid <46521756+TheProgrammingKid@users.noreply.github.com>
2019-01-09 17:10:37 +0530

    Update user_registration.py

--- registration/user_registration.py
+++ registration/user_registration.py
@@ -1,1 +1,19 @@
+import os
+from credential_manager import compiler
+
 class User(object):
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.path = os.path.dirname(os.path.abspath(__file__))
+        self.usernames_file = os.path.join(self.path, '.client_usernames', 'usernames.txt')
+        self.passwords_file = os.path.join(self.path, '.client_passwords', 'passwords.txt')
+        self.compiler = compiler.Compiler()
+        
+    def reegister(self):
+        with open(self.usernames_file) as f:
+            f.write(slf.username)
+            with open(self.paswords_file) as ff:
+                ff.write(self.compiler(self.password).compile())
+                ff.close()
+            f.close()
